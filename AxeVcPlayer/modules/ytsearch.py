732b81db9d9fb6b88081fa998b07eca028b77760
Avnish <83480217+Panther1999@users.noreply.github.com>
2021-09-13 17:51:53 +0530

    Add files via upload

---
+++ AxeVcPlayer/modules/ytsearch.py
@@ -0,0 +1,33 @@
+
+
+import logging
+
+from pyrogram import Client as app
+from pyrogram.types import Message
+from youtube_search import YoutubeSearch
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+import pyrogram
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+@app.on_message(pyrogram.filters.command(["search"]))
+async def ytsearch(_, message: Message):
+    try:
+        if len(message.command) < 2:
+            await message.reply_text("/search needs an argument!")
+            return
+        query = message.text.split(None, 1)[1]
+        m = await message.reply_text("Searching....")
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        i = 0
+        text = ""
+        while i += 1
+        await m.edit(text, disable_web_page_preview=True)
+    except Exception as e:
+        await message.reply_text(str(e))
